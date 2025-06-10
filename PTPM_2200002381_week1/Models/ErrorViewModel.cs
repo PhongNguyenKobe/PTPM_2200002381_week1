e9d9b2bc5e4814f9530d4f1be333f86a3232a567
PhongNguyenKobe <nguyendangthanhphong.lop8a7.23@gmail.com>
2025-06-10 09:25:04 +0700

    Add project files.

---
+++ PTPM_2200002381_week1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PTPM_2200002381_week1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
