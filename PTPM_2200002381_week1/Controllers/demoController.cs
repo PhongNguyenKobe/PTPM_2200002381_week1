5651ac5284c844f41bc4fd81ca1a5cf7a24f3cfd
PhongNguyenKobe <nguyendangthanhphong.lop8a7.23@gmail.com>
2025-06-14 11:20:11 +0700

    Day 2

---
+++ PTPM_2200002381_week1/Controllers/demoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PTPM_2200002381_week1.Controllers
+{
+    public class demoController : Controller
+    {
+        public string DemoA()
+        {
+            return "ngu";
+        }
+        
+        public ViewResult DemoB()
+        {
+            ViewBag.Message = "Hello from DemoB";
+            ViewBag.Mydata = "This is my data form DemoB"; 
+            return View("Demo3");
+        }
+
+
+        public ViewResult Demo3()
+        {
+            ViewBag.Message = "Hello from Demo3";
+            ViewBag.Mydata = "This is my data form Demo3"; 
+            return View();
+        }
+    }
+}
